7db2b5616b12b670c8e04ff1272ad6b235fcfbc0
oshanmanidu <oshanmanidu12@gmail.com>
2025-07-16 11:06:14 +0530

    2nd

---
+++ src/main/java/com/dev/DevLead/DevLead/DevLeadApplication.java
@@ -0,0 +1,13 @@
+package com.dev.DevLead.DevLead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevLeadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevLeadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/DevLead/DevLead/DevLeadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.DevLead.DevLead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevLeadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
