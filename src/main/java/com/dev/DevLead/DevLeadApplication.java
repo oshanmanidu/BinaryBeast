21e58267b3231ff63141aaea3b1b765c727dc330
Dilshan <dilshanwijerathna426@gmail.com>
2025-07-16 12:20:18 +0530

    Dilshan 3rd Commit

--- src/main/java/com/dev/DevLead/DevLeadApplication.java
+++ src/main/java/com/dev/DevLead/DevLeadApplication.java
@@ -1,4 +1,4 @@
-package com.dev.DevLead.DevLead;
+package com.dev.DevLead;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
